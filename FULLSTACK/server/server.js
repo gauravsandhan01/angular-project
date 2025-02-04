eefdf76be40c7c1ad61bf42c3f7e87faea8c7ace
Gaurav Sandhan <gaurav.sandhna01@gmail.com>
2025-02-04 14:52:53 +0530

    Initial commit

---
+++ FULLSTACK/server/server.js
@@ -0,0 +1,40 @@
+express =require('express');
+
+eobj = express();
+
+port = 5200;
+
+function Starter()
+{
+    console.log("Marvellous server is started at port 5200");
+}
+
+eobj.listen(port,Starter);
+
+function AcceptRequest(req,res)
+{
+    res.send("Marvellous server os NO...");
+}
+
+BalanceAmount=10000;
+
+eobj.get('/',AcceptRequest);
+
+function CreditAmount(req,res)
+{
+
+    value = req.query.Amount;
+    BalanceAmount=BalanceAmount+Number(value);
+    res.send("credit option is selected.."+BalanceAmount);
+}
+
+eobj.get('/Credit',CreditAmount);
+
+function DebitAmount(req,res)
+{
+    value = req.query.Amount;
+    BalanceAmount=BalanceAmount-Number(value);
+    res.send("Debit option is selected.."+BalanceAmount);
+}
+
+eobj.get('/Debit',DebitAmount);
